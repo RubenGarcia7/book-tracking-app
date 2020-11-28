6bb386073b69dabd1002c6340a0b6dfaf0dc615f
Ruben <hello@rubengarciabri.me>
2020-11-28 19:40:46 +0000

    Updates

---
+++ precache-manifest.73fd42cc3a1c167b4ea4600e49b4a462.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e44a74bd394456ae9f55",
+    "url": "./static/css/main.0543a375.chunk.css"
+  },
+  {
+    "revision": "e44a74bd394456ae9f55",
+    "url": "./static/js/main.e44a74bd.chunk.js"
+  },
+  {
+    "revision": "84f595fa1e954758c99d",
+    "url": "./static/js/1.84f595fa.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "./static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "./static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "./static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "0c50b28f30339a2b3241b8d4abe02e7a",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/RubenGarcia7/book-tracking-app/precache-manifest.d361e80d6b15ed42377be69f6a91dc1c.js"
+  "./precache-manifest.73fd42cc3a1c167b4ea4600e49b4a462.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/RubenGarcia7/book-tracking-app/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
